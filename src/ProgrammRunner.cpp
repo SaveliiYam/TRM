b3a5b3414119fafb14e5139d59d353209da65f84
SaveliiYam <savelkayamil5@gmail.com>
2023-08-11 10:14:33 +0300

    10:14

---
+++ src/ProgrammRunner.cpp
@@ -0,0 +1,43 @@
+#include "ProgrammRunner.h"
+
+ProgrammRunner::ProgrammRunner(const temperaturePausesStruct &programm, const bool &time, const bool &delay)
+    : programm_(programm), timeSet(time), timeDelay(delay) {}
+ProgrammRunner::ProgrammRunner(const bool &time, const bool &delay) : timeSet(time), timeDelay(delay) {}
+ProgrammRunner::ProgrammRunner() {}
+Pair<byte, uint16_t> ProgrammRunner::runningProgramm(const float &temperature)
+{
+
+    if (!timeDelay || temperature >= (programm_.setpointTemperature[numberPause] - 1) && !can_to_start)
+    {
+        can_to_start = true;
+        startTime = true;
+        startPause();
+    }
+    else
+    {
+        startTime = false;
+    }
+
+    if (startTime && timer + programm_.time[numberPause] >= millis()) // если отсчет закончен
+    {
+        Pair<byte, uint16_t> res{numberPause, programm_.time[numberPause]};
+        can_to_start = false;
+        numberPause++;
+        if (numberPause == 6)
+        {
+            numberPause = 0;
+            programm_stop();
+        }
+        return res;
+    }
+    else if (startTime && timer + programm_.time[numberPause] < millis()) // если отсчет еще не закончен
+    {
+        Pair<byte, uint16_t> res{numberPause, programm_.time[numberPause]};
+        return res;
+    }
+    else if (!startTime) // если еще не начался отсчет
+    {
+        Pair<byte, uint16_t> res{numberPause, programm_.time[numberPause]};
+        return res;
+    }
+}
--- src/ProgrammRunner.h
+++ src/ProgrammRunner.h
@@ -7,7 +7,6 @@ class ProgrammRunner
 {
 private:
     temperaturePausesStruct programm_;
-
     bool timeSet = false;
     bool timeDelay = false;
     bool startTime = false;
@@ -18,10 +17,9 @@ private:
     byte numberPause = 0;
 
 public:
-    ProgrammRunner(const temperaturePausesStruct &programm, const bool &time, const bool &delay)
-        : programm_(programm), timeSet(time), timeDelay(delay) {}
-    ProgrammRunner(const bool &time, const bool &delay) : timeSet(time), timeDelay(delay) {}
-    ProgrammRunner(){}
+    ProgrammRunner(const temperaturePausesStruct &programm, const bool &time, const bool &delay);
+    ProgrammRunner(const bool &time, const bool &delay);
+    ProgrammRunner();
     void putTimeSet(const bool &time) { timeSet = time; }
     void putTimeDelay(const bool &delay) { timeDelay = delay; }
     void putTimeSettings(const bool &time, const bool &delay)
@@ -30,49 +28,13 @@ public:
         timeDelay = delay;
     }
     void putProgramm(const temperaturePausesStruct &programm) { programm_ = programm; }
-    bool is_programm_run() { return is_stopped; } // если программа закончена, то false
+    bool is_programm_run() const { return is_stopped; } // если программа закончена, то false
     void programm_stop() { is_stopped = false; }
     void startPause()
     {
         timer = millis();
         ticker = programm_.time[numberPause];
     }
-
-    Pair<byte, uint16_t> runningProgramm(const float &temperature)
-    {
-
-        if (!timeDelay || temperature >= (programm_.setpointTemperature[numberPause] - 1) && !can_to_start)
-        {
-            can_to_start = true;
-            startTime = true;
-            startPause();
-        }
-        else
-        {
-            startTime = false;
-        }
-
-        if (startTime && timer + programm_.time[numberPause] >= millis()) // если отсчет закончен
-        {
-            Pair<byte, uint16_t> res{numberPause, programm_.time[numberPause]};
-            can_to_start = false;
-            numberPause++;
-            if (numberPause == 6)
-            {
-                numberPause = 0;
-                programm_stop();
-            }
-            return res;
-        }
-        else if (startTime && timer + programm_.time[numberPause] < millis()) // если отсчет еще не закончен
-        {
-            Pair<byte, uint16_t> res{numberPause, programm_.time[numberPause]};
-            return res;
-        }
-        else if (!startTime) // если еще не начался отсчет
-        {
-            Pair<byte, uint16_t> res{numberPause, programm_.time[numberPause]};
-            return res;
-        }
-    }
+    Pair<byte, uint16_t> runningProgramm(const float &temperature);
 };
+
