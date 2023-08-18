d4130976b97fe7c5ef8d039a1af200e23c41bd84
SaveliiYam <savelkayamil5@gmail.com>
2023-08-18 13:11:25 +0300

    13:11

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
-
-
 #include "TRM.h"
 #include "LCD.h"
 #include <LiquidCrystal_I2C.h>
 // #include <ThingerESP32.h>
 // #include "arduino_secrets.h"
 // #define TINGER_SERIAL_DEBUG
-//ThingerESP32 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
-int i = 0;
+// ThingerESP32 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
+int a = 0, b;
+bool high_low = false;
+uint32_t timer = millis();
 
 const byte input_pin = 25;
 const byte output_pin = 26;
@@ -21,11 +21,6 @@ const byte sck = 12;
 const byte cs = 14;
 const byte so = 27;
 const byte mtrRelay = 14;
-
-    //TRM(const byte& dwnBtn, const byte& upBtn, const byte& setBtn, const byte& strtBtn,
-    //    const byte& nmbBtn, const byte& mtrBtn, const byte& mtrPin , const byte& sckPin, const byte& csPin, const byte& soPin);
-
-
 
 void setup()
 {
@@ -44,61 +39,35 @@ void setup()
     lcd.print("hi");
 
     delay(500);
-    //LCD lcdClass(lcd);
+    // LCD lcdClass(lcd);
+    pinMode(output_pin, OUTPUT);
+    digitalWrite(output_pin, LOW);
 
-
-TRM trm{downButton, upButton, settingsButtn, startButton, numberButton, mtrButton, mtrRelay, sck, cs, so};
+    TRM trm{downButton, upButton, settingsButtn, startButton, numberButton, mtrButton, mtrRelay, sck, cs, so};
     while (1)
     {
         trm.main_programm();
+        
+        a = trm.getPIDvalue();
+        b = map(a, 0, 255, 255, 0);
 
+        if (!high_low && (millis() - timer >= b))
+        {
+            digitalWrite(output_pin, HIGH);
+            timer = millis();
+            high_low = true;
+        }
+        if (high_low && (millis() - timer >= a))
+        {
+            digitalWrite(output_pin, LOW);
+            timer = millis();
+            high_low = false;
+        }
+        //Serial.print("PID value: ");
+        //Serial.println(a);
     }
-
-    
 }
 
 void loop()
 {
 }
-
-
-
-
-/*
-// тест одноканального диммера
-// библиотека универсальная, поэтому требуется свой таймер
-// в этом примере делаем на микросе!
-#include <Arduino.h>
-
-int a = 20, b;
-bool high_low = false;
-uint32_t timer = millis();
-
-void setup(){
-    pinMode(26, OUTPUT);
-}
-
-void loop(){
-    //a-=5;
-    // b+=5;
-
-    if(a == 0){a = 255;}
-    b = map(a, 0, 255, 255, 0);
-
-    if(high_low && (millis() - timer >= b)){
-        digitalWrite(26, HIGH);
-        timer = millis();
-        high_low = false;
-    }
-    if(!high_low && (millis() - timer >= a)){
-        digitalWrite(26, LOW);
-        timer = millis();
-        high_low = true;
-    }
-
-    
-}
-*/
-
-
-
